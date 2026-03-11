61101175f1219de0c72780257310cb0fe6641c92
Brandon Araujo <bac2795@gmail.com>
2026-03-11 18:05:52 -0500

    Fix

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 locals {
-  idapp = "baraujo02" # baraujo02
+  idapp = "baraujo01" # baraujo01
 }
 
 terraform {
@@ -13,6 +13,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "3d4bb8be-b12c-4c52-8cec-1b81cb8fb1a0" # Id de suscripción DMC
+  subscription_id = "b497fd69-266c-46a9-b55b-8be0cd579667" # Id de suscripción DMC
   # subscription_id = "f2a08fd0-079c-4dab-a431-df74b86366ab" # Id de suscripción Personal
 }
